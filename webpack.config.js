95d436f1c9a5e49f26a9472a04df79d93683afa2
acidd3 <acidd3@mail.ru>
2019-02-10 23:03:38 +0300

    webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,52 @@
-{
-  "scripts": {
-    "start": "webpack"
-  },
-  "devDependencies": {
-    "webpack": "^2.2.1"
-  }
-}
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+	source: path.join(__dirname, 'source'),
+	build: path.join(__dirname, 'build')
+};
+
+const common = {
+    entry: PATHS.source + '/index.js',
+    output: {
+        path: PATHS.build,
+        filename: '[name].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: PATHS.source + '/index.pug',
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader',
+                options: {
+                    pretty: true
+                }
+            }
+        ]
+    
+    }
+};
+
+const developmentConfig = {
+    devServer: {
+        stats: 'errors-only',
+        port: '9000'
+    }
+};
+
+module.exports = function(env) {
+    if (env === 'production'){
+        return common;
+    }
+    if (env === 'development'){
+        return Object.assign(
+            {},
+            common,
+            developmentConfig
+            )
+    }
+};
